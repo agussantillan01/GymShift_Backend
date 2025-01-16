99da1f42706200dc0b05b1d2b8c29e5f63902b43
agussantillan01 <agusantillan01@hotmail.com>
2025-01-15 21:30:22 -0300

    AS - Agrego clases de excepciones

---
+++ Business/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : base("Ocurrieron uno o más errores de validacion.")
+        {
+            Errors = new List<string>();
+
+        }
+        public List<string> Errors { get; }
+        public List<ValidationException> ValidationFailures { get; set; }
+
+        public ValidationException(IEnumerable<ValidationException> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.Message);
+            }
+            ValidationFailures = failures.ToList();
+        }
+    }
+}
