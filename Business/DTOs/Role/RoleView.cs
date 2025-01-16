716287919ddf72a7b6942328bb31a7c848cc212d
agussantillan01 <agusantillan01@hotmail.com>
2025-01-15 21:29:10 -0300

    AS - Agrego clases de roles

---
+++ Business/DTOs/Role/RoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DTOs.Role
+{
+    public class RoleView
+    {
+        public Infrastructure.Models.Role? Roles { get; set; }
+        public List<Infrastructure.Models.Permission> ListPermissions { get; set; }
+    }
+}
---
+++ Business/DTOs/Role/UserRolesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DTOs.Role
+{
+    public class UserRolesDTO
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
